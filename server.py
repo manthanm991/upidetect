c412f7b382c8f2f0e4064170065ee3d117a35957
Darshan <joshi.darshan1826@gmail.com>
2024-03-17 01:41:07 +0530

    added boolean function to the server.py class again

--- server.py
+++ server.py
@@ -4,28 +4,42 @@ import pickle
 app = Flask(__name__)
 
 
-with open('UPIFraudDetectionModel.pkl','rb') as f:
-    model = pickle.load(f)
+# with open('UPIFraudDetectionModel.pkl','rb') as f:
+#     model = pickle.load(f)
 
 
-@app.route('/predict',methods =['POST'])
-def predict():
-    data = request.json
+# @app.route('/predict',methods =['POST'])
+# def predict():
+#     data = request.json
 
-    features = [
-        data['type'],
-        data['amount'],
-        data['oldbalanceOrg'],
-        data['newbalanceOrig'],
-        data['oldbalanceDest'],
-        data['newbalanceDest']
-    ]
+#     features = [
+#         data['type'],
+#         data['amount'],
+#         data['oldbalanceOrg'],
+#         data['newbalanceOrig'],
+#         data['oldbalanceDest'],
+#         data['newbalanceDest']
+#     ]
     
-    features = [features]
+#     features = [features]
 
-    prediction = model.predict
+#     prediction = model.predict
 
-    return jsonify({'prediction': int(prediction[0])})
+#     return jsonify({'prediction': int(prediction[0])})
+
+boolGenerator = BooleanGenerator()
+
+
+
+@app.route('/gitBooleanValue',methods = ['GET'])
+def getBoolean():
+    result = boolGenerator.generate_boolean()
+
+    return jsonify({'boolean_value': result})
+
+
+
+    
 
 
 if __name__ == '__main__':
